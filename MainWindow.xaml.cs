1661d1429301e1514ad2be9e1bee99ec82f1cc40
Александр Кузнецов <yakush4610@gmail.com>
2022-09-22 17:17:30 +1000

    Create Button and by cliking "Hy ISP"

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WPF_Kuznetsov_PR1
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Привет, группа ИСП!");
+        }
     }
 }
